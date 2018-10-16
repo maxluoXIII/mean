f12c8fe77abd94704dc68b7a164af14ca6603608
Takeshi Suzuki <takeshibaconsuzuki@users.noreply.github.com>
2018-10-15 20:23:23 -0500

    Update mean.py

--- mean.py
+++ mean.py
@@ -1,9 +1,2 @@
 def mean(num_list):
-    try:
-        return sum(num_list)/len(num_list)
-    except ZeroDivisionError :
-        return 0
-    except TypeError as detail :
-        msg = "The algebraic mean of an non-numerical list is undefined.\
-               Please provide a list of numbers."
-        raise TypeError(detail.__str__() + "\n" +  msg)
+    raise ValueError('You expect me to compute shit?')
